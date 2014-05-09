af8f442ddddf631e94219936c610f4a46b76bedc
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-09 08:59:36 +0200

    Windows client

---
+++ WIndowsClient/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Windows.Forms;
+using WebApiSignalRAngularDemo;
+
+namespace WIndowsClient
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:41359/api/");
+
+            var result = await client.GetAsync("products");
+            var products = await result.Content.ReadAsAsync<List<Product>>();
+
+            listBox1.DisplayMember = "Title";
+            listBox1.DataSource = products;
+        }
+    }
+}
--- WebApiSignalRAngularDemo/Startup.cs
+++ WebApiSignalRAngularDemo/Startup.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Owin;
 using System.Web.Http;
-using Owin;
 
 namespace WebApiSignalRAngularDemo
 {
