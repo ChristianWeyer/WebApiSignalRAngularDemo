f42ec5186c426e1ac1d0cb2bc9faf0617415957f
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-09 13:09:04 +0200

    Live...

--- WebApiSignalRAngularDemo/products/productsList.js
+++ WebApiSignalRAngularDemo/products/productsList.js
@@ -1,7 +1,17 @@
-﻿myApp.controller("productsList", function($scope, productsService) {
+﻿myApp.controller("productsList", function($scope, $http, productsService) {
     //productsService.init($scope);
 
-    var products = [{ Id: 1, Title: "Viedell Wassa", Description: "Very legga whahda", Amount: 14 }, { Id: 2, Title: "Schio Dschibbs", Description: "Guad abba ungsunnd", Amount: 9 }, { Id: 3, Title: "Schogglaad leid", Description: "Schmeggt e bissl komisch - abba naja", Amount: 249 }];
+    //var products = [
+    //    { Id: 1, Title: "Viedell Wassa", Description: "Very legga whahda", Amount: 14 },
+    //    { Id: 2, Title: "Schio Dschibbs", Description: "Guad abba ungsunnd", Amount: 9 },
+    //    { Id: 3, Title: "Schogglaad leid", Description: "Schmeggt e bissl komisch - abba naja", Amount: 249 }];
 
-    $scope.products = products;
+
+    $http.get("/api/products").then(function(result) {
+        $scope.products = result.data;
+    }, function (error) {
+        alert(JSON.stringify(error));
+    });
+
+    
 });
