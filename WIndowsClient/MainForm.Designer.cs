1a10a8891560e5376064e412bddab6c66c40fe82
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-09 10:39:07 +0200

    SignalR Windows client

--- WIndowsClient/MainForm.Designer.cs
+++ WIndowsClient/MainForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +51,22 @@
             this.listBox1.Size = new System.Drawing.Size(426, 186);
             this.listBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Subscribe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(450, 349);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "MainForm";
@@ -67,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WIndowsClient/MainForm.cs
+++ WIndowsClient/MainForm.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Windows.Forms;
+using Microsoft.AspNet.SignalR.Client;
 using WebApiSignalRAngularDemo;
 
 namespace WIndowsClient
 {
     public partial class MainForm : Form
     {
+
         public MainForm()
         {
             InitializeComponent();
@@ -23,6 +25,17 @@ namespace WIndowsClient
 
             listBox1.DisplayMember = "Title";
             listBox1.DataSource = products;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var hubConnection = new HubConnection("http://localhost:41359");
+
+            var chat = hubConnection.CreateHubProxy("pokeHub");
+            chat.On<string>("poke", msg => this.Invoke(
+                new Action(() => MessageBox.Show(msg))));
+
+            await hubConnection.Start();
         }
     }
 }
