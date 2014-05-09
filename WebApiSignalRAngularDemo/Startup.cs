d5c735b8acb7ad98dae9004f9d32218b4c6ba8d2
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-09 08:48:10 +0200

    Products Web API

---
+++ WebApiSignalRAngularDemo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace WebApiSignalRAngularDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}");
+
+            app.UseWebApi(config);
+        }
+    }
+}
