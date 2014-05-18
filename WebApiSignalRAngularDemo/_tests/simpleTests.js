78e8013561d2bc0580958c87537c36073119f710
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-18 22:52:45 +0200

    Added very simple sample Jasmine test

---
+++ WebApiSignalRAngularDemo/_tests/simpleTests.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="_references.js" />
+
+describe('Controller: Products List', function () {
+    var productsCtrl, scope;
+
+    beforeEach(module('myApp'));
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        productsCtrl = $controller('productsListController', {
+            $scope: scope
+        });
+    }));
+
+    it('should have scope defined', function () {
+        expect(scope).toBeDefined();
+    });
+});
