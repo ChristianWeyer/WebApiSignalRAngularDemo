669469f9b8b2a0c4d823747d7647244ca29d6822
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-18 16:46:45 +0200

    No Web API and SignalR

---
+++ WebApiSignalRAngularDemo/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace WebApiSignalRAngularDemo.Server
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.Bundles.Add(new StyleBundle("~/app/css")
+               .IncludeDirectory("~/libs", "*.css", true)
+               .IncludeDirectory("~/app", "*.css", true)
+               );
+
+            BundleTable.Bundles.Add(new ScriptBundle("~/app/js")
+                .IncludeDirectory("~/libs", "*.js", true)
+                .IncludeDirectory("~/app", "*.js", true)
+                );
+        }
+    }
+}
--- WebApiSignalRAngularDemo/Global.asax.cs
+++ WebApiSignalRAngularDemo/Global.asax.cs
@@ -1,19 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Web.Optimization;
+using WebApiSignalRAngularDemo.Server;
 
 namespace WebApiSignalRAngularDemo
 {
     public class Global : System.Web.HttpApplication
     {
-
         protected void Application_Start(object sender, EventArgs e)
         {
-            
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-
     }
 }
