fb25438ba893817dc9b23f9a2a6dd74d3441ca88
Christian Weyer <christian.weyer@thinktecture.com>
2014-05-21 07:17:30 +0100

    Client cleanup

--- WebApiSignalRAngularDemo/app/services/productsService.js
+++ WebApiSignalRAngularDemo/app/services/productsService.js
@@ -1,22 +1,10 @@
 ﻿myApp.factory("productsService", function ($http) {
     var service = {};
 
-    var connection = $.hubConnection();
-    connection.logging = true;
-
-    var hubProxy = connection.createHubProxy("pokeHub");
-    
     service.init = function (scope) {
-        hubProxy.on("poke", function (data) {
-            scope.$broadcast("productsPoke", data);
-        });
-
-        connection.start().done(function () {
-        });
     }
 
     service.listProducts = function() {
-        return $http.get("/api/products");
     };
 
     return service;
