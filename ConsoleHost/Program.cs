2ed56cb7577645d6f323c056f5738f0197aaefd5
Christian Weyer <christian.weyer@thinktecture.com>
2015-02-24 08:18:52 +0100

    Removed Nugets

--- ConsoleHost/Program.cs
+++ ConsoleHost/Program.cs
@@ -8,11 +8,11 @@ namespace ConsoleHost
     {
         static void Main(string[] args)
         {
-            using (var server = WebApp.Start<Startup>("http://localhost:9999"))
-            {
-                Console.WriteLine("Web API server running...");
-                Console.ReadLine();
-            }
+            //using (var server = WebApp.Start<Startup>("http://localhost:9999"))
+            //{
+            //    Console.WriteLine("Web API server running...");
+            //    Console.ReadLine();
+            //}
         }
     }
 }
